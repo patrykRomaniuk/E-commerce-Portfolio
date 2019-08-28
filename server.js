3cc120cd68326bccdc95e9c897094141aa46ad22
Patryk <patryk-romaniuk@o2.pl>
2019-08-28 11:56:26 +0200

    Added production handler

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ connectDatabase();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/hearts',require('./routes/hearts'));
 app.use('/api/shopItems',require('./routes/shopItems'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
