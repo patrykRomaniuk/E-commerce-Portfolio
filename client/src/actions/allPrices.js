e7015c1a34511fd177501b3ffa970d9789eacf41
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-02-08 19:31:03 +0100

    Commented getSumPrices action

--- client/src/actions/allPrices.js
+++ client/src/actions/allPrices.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { GET_SUM } from './constants';
- 
+
+//Getting sum from prices
 export const getSumPrices = () => async dispatch => {
     try {
+        //Request to backend
         const res = await axios.get('https://obscure-wave-98753.herokuapp.com/api/shopItems/sum_prices');
+        //Dispatching action
         dispatch({
             type: GET_SUM,
             payload: res.data
