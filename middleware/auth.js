4e892635838a8cd77fdeff4ff4537595f8561c45
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-02-08 19:28:16 +0100

    Commented auth.js middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req,res,next){
     try {
+        //Getting token from header
         const token = req.header('x-auth-token');
+        //Verifying token
         const decoded = jwt.verify(
             token,
             config.get('jwtSecret')
         );
+        //Adding value to request
         req.user = decoded.user;
         next();
     } catch (error) {
