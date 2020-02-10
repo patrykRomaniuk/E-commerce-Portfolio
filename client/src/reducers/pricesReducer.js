2d01e3f3a75eccca6b99b368987c0550b09dd377
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-02-10 20:08:00 +0100

    Commented prices reducer:

--- client/src/reducers/pricesReducer.js
+++ client/src/reducers/pricesReducer.js
@@ -1,12 +1,15 @@
 import { GET_SUM } from "../actions/constants";
 
+//Initialized reducer
 const initialState = {
     allPricesValue: 0
 };
 
 const prices = (state = initialState,action) => {
+    //Get type and payload from action
     const { type,payload } = action;
     switch(type){
+        //Got sum
         case GET_SUM:
             return{
                 ...state,
