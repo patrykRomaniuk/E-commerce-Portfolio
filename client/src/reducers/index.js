eed69aed9551d1182a509c4b89b3de70b405b4ee
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-01 14:39:11 +0100

    Commented combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import auth from './authReducer.js';
 import prices from './pricesReducer';
 
+//Combining reducers
 export default combineReducers({
     auth,
     prices
