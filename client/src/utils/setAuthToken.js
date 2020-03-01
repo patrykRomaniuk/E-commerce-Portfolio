e61885a9e33c26fa793e22b2aae9307dd686dfc2
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-01 14:47:47 +0100

    Added comment to setAuthToken

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const setAuthToken =  token => {
     //Checking if there is token
     if(token) axios.defaults.headers.common['x-auth-token'] = token;
+    //If not - delete token from x-auth-token
     else delete axios.defaults.headers.common['x-auth-token'];
 }
 
