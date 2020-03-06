f3a62c4510417227f24a3a077d9404b545ef46d8
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-06 18:31:25 +0100

    Commented connectToDatabase function

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDatabase = async () => {
     try {
+        //Connecting to database 
         await mongoose.connect(
             db,
             { 
@@ -13,6 +14,7 @@ const connectDatabase = async () => {
                 useFindAndModify: true
             }
         );
+        //Message if application is connected
         console.log('MongoDB is connected');
     } catch (error) {
         console.log(error.message);
