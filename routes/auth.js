b34f4ab0e332e1e6e240f90d4d8b4b97c42b2a3f
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2021-06-05 16:29:43 +0200

    Removed redundant comments in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,41 +9,31 @@ const bcryptjs = require('bcryptjs');
 //Login system
 router.post(
     '/',
-    //Express-validator
     [
         check('email','Please Put Valid Email').isEmail(),
         check('password','Password is required').not().isEmpty()
     ],
     async(req,res) => {
-        //Taking values from body
         const { email,password } = req.body;
         const errors = validationResult(req);
-        //Searching for user by email
         let user = await User.findOne({ email });
-        //Checking if there is not user
         if(!user){
             return res.status(404).json({ msg: "User not found" });
         }
-        //Error handler
         if(!errors.isEmpty()){
             return res.status(400).json({ errors: errors.array() });
         }
-        
         try {
-            //Checking if passwords match
             const isMatch = await bcryptjs.compare(password,user.password);
             if(!isMatch){
                 return res.status(401).json({ msg: "Password does not match" });
             }
-            //Saving user
             await user.save();
-            //Initalizing payload
             const payload = {
                 user: {
                     id: user.id
                 }
             };
-            //Adding token
             jwt.sign(
                 payload,
                 config.get('jwtSecret'),
