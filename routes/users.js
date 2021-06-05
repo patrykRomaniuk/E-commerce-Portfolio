d2ad72b1ba59a73273b267abee92d682be48b79d
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2021-06-05 16:34:08 +0200

    Removed redundant comments from users.js

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,10 @@ const { validationResult,check } = require('express-validator');
 //Getting user
 router.get(
     '/me',
-    //Checking if is loggedi n
     auth,
     async(req,res) => {
         try {
-            //Getting user without password
             let user = await User.findById(req.user.id).select('-password');
-            //Output
             res.json(user);
         } catch (error) {
             console.log(error.message);
@@ -36,27 +33,20 @@ router.post(
         check('password','Password is required').not().isEmpty(),
     ],  
     async(req,res) => {
-        //Getting values
         const { name,last_name,username,email,password } = req.body;
-        //Getting user by Email
         let validateByUserEmail = await User.findOne({ email });
-        //Getting user by Name
         let validateByUsername = await User.findOne({ username });
-        //Checking if there is user with this name
         if(validateByUsername){
             return res.status(401).json({ msg: "User with that username already exists" });
         }
-        //Checking if there is user with this e-mail
         if(validateByUserEmail){
             return res.status(401).json({ msg: "User with that e-mail already exists" });
         }
-        //Error handler
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(500).json({ errors: errors.array() })
         }
         try {
-            //Initializing user
             const user = new User({
                 name,
                 last_name,
@@ -64,19 +54,14 @@ router.post(
                 email,
                 password
             });
-            //Added salt
             const salt = await bcryptjs.genSalt(10);
-            //Hashed user password
             user.password = await bcryptjs.hash(password,salt);
-            //Saving to database
             await user.save();
-            //Payload
             const payload = {
                 user: {
                     id: user.id
                 }
             };
-            //Adding token
             jwt.sign(
                 payload,
                 config.get('jwtSecret'),
